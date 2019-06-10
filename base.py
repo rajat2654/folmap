f8c7036147840d967801e012a5ba455dc1f5d212
rajat2654 <48014274+rajat2654@users.noreply.github.com>
2019-06-10 13:13:31 +0530

    Add files via upload

--- base.py
+++ base.py
@@ -7,15 +7,18 @@ dd = pandas.read_csv("Volcanoes.txt")
 s1 = list(dd["LAT"])
 s2 = list(dd["LON"])
 el = list(dd["ELEV"])
+name = list(dd["NAME"])
 
 html = """<h4>Volcano info</h4>
+Name = 
+<a href="https://www.google.com/search?q==%%22%s%%22" target="_blank">%s</a><br>
 Height = %s m
 """
 
 for i in range(len(dd)):
-    ifr = folium.IFrame(html=html %str(el[i]), height=100, width=200)
+    ifr = folium.IFrame(html=html %(name[i], name[i], str(el[i])), height=100, width=200)
     fg.add_child(folium.Marker(location=[s1[i], s2[i]], popup=folium.Popup(ifr), icon=folium.Icon(color="blue")))
 
 mapp.add_child(fg)
 
-mapp.save("map_simple_html.html")
+mapp.save("map_advanced_html.html")
