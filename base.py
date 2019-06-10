7d38c4753b7413d60b0593dbd5b963e221f18887
rajat2654 <48014274+rajat2654@users.noreply.github.com>
2019-06-10 12:49:11 +0530

    Add files via upload

--- base.py
+++ base.py
@@ -8,9 +8,14 @@ s1 = list(dd["LAT"])
 s2 = list(dd["LON"])
 el = list(dd["ELEV"])
 
+html = """<h4>Volcano info</h4>
+Height = %s m
+"""
+
 for i in range(len(dd)):
-    fg.add_child(folium.Marker(location=[s1[i], s2[i]], popup="Elevation of this place : " + str(el[i]) + " m", icon=folium.Icon(color="blue")))
+    ifr = folium.IFrame(html=html %str(el[i]), height=100, width=200)
+    fg.add_child(folium.Marker(location=[s1[i], s2[i]], popup=folium.Popup(ifr), icon=folium.Icon(color="blue")))
 
 mapp.add_child(fg)
 
-mapp.save("map.html")
+mapp.save("map_simple_html.html")
